2a36dee212dd638a06445ac4d617828400f4826d
K.R. Zentner <krzentner@gmail.com>
2022-06-22 01:00:35 -0700

    Generate voxels from observations

--- src/metaworld_3d_env.py
+++ src/metaworld_3d_env.py
@@ -1,5 +1,10 @@
 from metaworld.envs import ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE
 import numpy as np
+import gym.spaces as spaces
+import gym
+
+import plotly.express as px
+import plotly.graph_objects as go
 
 def render_env(env_name, seed=1000):
     env = ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE[f'{env_name}-v2-goal-observable'](seed)
@@ -51,61 +56,159 @@ def render_env(env_name, seed=1000):
 def points3d(x, y, z, *args, **kwargs):
     return mlab.points3d(100 * x, 100 * y, 100 * z, *args, **kwargs)
 
-def get_camera_mat(env, width, height, camera_name):
-    pos = env.sim.data.get_camera_xpos(camera_name)
-    rot = env.sim.data.get_camera_xmat(camera_name).reshape(3, 3).T
-    # HMM, how to get cam_id from camera_name
-    # fov = env.sim.model.cam_fovy[cam_id]
-    # print(env.sim.model.cam_fovy)
-    # Just hard code the camera fov
-    assert camera_name == 'corner4'
-    fov = 30
+from mj_pc.mj_point_clouds import PointCloudGenerator
+import open3d as o3d
 
-    # Translation matrix (4x4).
-    translation = np.eye(4)
-    translation[0:3, 3] = -pos
-    # Rotation matrix (4x4).
-    rotation = np.eye(4)
-    rotation[0:3, 0:3] = rot
-    # Focal transformation matrix (3x4).
-    focal_scaling = (1./np.tan(np.deg2rad(fov)/2)) * height / 2.0
-    focal = np.diag([-focal_scaling, focal_scaling, 1.0, 0])[0:3, :]
-    # Image matrix (3x3).
-    image = np.eye(3)
-    image[0, 2] = (width - 1) / 2.0
-    image[1, 2] = (height - 1) / 2.0
-    camera_matrix = image @ focal @ rotation @ translation
-    return camera_matrix
+def render_env_mj_pc(env_name, seed=1000):
+    env = MetaWorldVoxelEnv(env_name=env_name, seed=seed)
+    obs = env.reset()
+    assert env.observation_space['imgs']['depth_imgs'].contains(obs['imgs']['depth_imgs'])
+    assert env.observation_space['imgs']['color_imgs'].contains(obs['imgs']['color_imgs'])
+    assert env.observation_space['imgs']['cam_locations'].contains(obs['imgs']['cam_locations'])
+    assert env.observation_space['imgs']['cam2world_mats'].contains(obs['imgs']['cam2world_mats'])
+    # Don't check low_dim, since it's broken anyways
+    env.obsToNumpyVoxels(obs)
 
-import pylab as pl
-from mayavi import mlab
+class MetaWorldVoxelEnv(gym.Env):
 
-def mlab_imshowColor(im, alpha=255, **kwargs):
-    """
-    Plot a color image with mayavi.mlab.imshow.
-    im is a ndarray with dim (n, m, 3) and scale (0->255]
-    alpha is a single number or a ndarray with dim (n*m) and scale (0->255]
-    **kwargs is passed onto mayavi.mlab.imshow(..., **kwargs)
-    """
-    try:
-        alpha[0]
-    except:
-        alpha = pl.ones(im.shape[0] * im.shape[1]) * alpha
-    if len(alpha.shape) != 1:
-        alpha = alpha.flatten()
+    def __init__(self,
+                 *,
+                 env_name,
+                 seed,
+                 camera_names=('corner4', 'behindGripper'),
+                 img_width=128,
+                 img_height=72):
+        self.img_width = img_width
+        self.img_height = img_height
+        self.env = ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE[f'{env_name}-v2-goal-observable'](seed)
+        self.env.seeded_rand_vec = True
+        self.pc_gen = PointCloudGenerator(self.env.sim,
+                                          min_bound=(-1., -1., -1.),
+                                          max_bound=(1., 1., 1.),
+                                          camera_names=camera_names,
+                                          width=img_width,
+                                          height=img_height)
+        self.action_space = self.env.action_space
+        self.observation_space = spaces.Dict({
+            'low_dim': self.env.observation_space,
+            'imgs': spaces.Dict({
+                'cam2world_mats': spaces.Dict({
+                    cam_name: spaces.Box(low=-np.inf, high=np.inf, shape=(4, 4))
+                    for cam_name in camera_names
+                }),
+                'cam_locations': spaces.Dict({
+                    cam_name: spaces.Box(low=-np.inf, high=np.inf, shape=(3,))
+                    for cam_name in camera_names
+                }),
+                'color_imgs': spaces.Dict({
+                    cam_name: spaces.Box(low=0, high=255,
+                                         shape=(self.img_height, self.img_width, 3),
+                                         dtype=np.uint8)
+                    for cam_name in camera_names
+                }),
+                'depth_imgs': spaces.Dict({
+                    cam_name: spaces.Box(low=0, high=np.inf,
+                                         shape=(self.img_height, self.img_width),
+                                         dtype=np.float32)
+                    for cam_name in camera_names
+                }),
+            })
+        })
 
-    # The lut is a Nx4 array, with the columns representing RGBA
-    # (red, green, blue, alpha) coded with integers going from 0 to 255,
-    # we create it by stacking all the pixles (r,g,b,alpha) as rows.
-    myLut = pl.c_[im.reshape(-1, 3), alpha]
-    myLutLookupArray = pl.arange(im.shape[0] * im.shape[1]).reshape(im.shape[0], im.shape[1])
+    def _wrap_obs(self, low_dim_obs):
+        return {'low_dim': low_dim_obs,
+                'imgs': self.pc_gen.generateObservation()}
 
-    #We can display an color image by using mlab.imshow, a lut color list and a lut lookup table.
-    theImshow = mlab.imshow(myLutLookupArray, colormap='binary', **kwargs) #temporary colormap
-    theImshow.module_manager.scalar_lut_manager.lut.table = myLut
-    mlab.draw()
+    def reset(self):
+        obs = self.env.reset()
+        return self._wrap_obs(obs)
 
-    return theImshow
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        return self._wrap_obs(obs), reward, done, info
+
+    def visualize(self, obs):
+        point_cloud = self.pc_gen.observationToPointCloud(obs['imgs'],
+                                                          estimate_normals=True)
+        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(point_cloud,
+                                                                    voxel_size=0.01)
+        o3d.visualization.draw_geometries([point_cloud, voxel_grid])
+
+    VOXEL_FEATURES = 5 # RGB + Count + Ones
+
+    def obsToNumpyVoxels(self, obs, grid_shape=(64, 64, 64), grid_dims=(1., 1., 1.)):
+        """
+
+        Args:
+            obs (dict): Observation to turn into a voxel grid.
+            grid_shape (int, int, int): Shape of grid (not counting feature dimensions).
+            grid_dims (float, float, float): Bounds to clip the points to
+
+        Returns:
+            np.ndarray: The featurized voxel grid. Has shape grid_shape + (VOXEL_FEATUES,)
+
+        """
+        gripper_location = obs['low_dim'][:3]
+        point_cloud = self.pc_gen.observationToPointCloud(obs['imgs'],
+                                                          estimate_normals=False)
+        # Center the gripper
+        # That's right, it's serious egocentrism time :sunglasses:
+        point_cloud = point_cloud.translate(-gripper_location)
+
+        min_bound = (-grid_dims[0] / 2, -grid_dims[1] / 2, -grid_dims[2] / 2)
+        max_bound = (grid_dims[0] / 2, grid_dims[1] / 2, grid_dims[2] / 2)
+        target_bounds = o3d.geometry.AxisAlignedBoundingBox(
+            min_bound=min_bound, max_bound=max_bound)
+        point_cloud = point_cloud.crop(target_bounds)
+
+        locations = np.asarray(point_cloud.points)
+        colors = np.asarray(point_cloud.colors, dtype=np.float32) / 255.0
+        indices = np.trunc((locations - min_bound) * grid_shape).astype(np.uint32)
+        count_grid = np.zeros(shape=grid_shape, dtype=np.uint32)
+        color_grid = np.zeros(shape=grid_shape + (3,), dtype=np.float32)
+        for i in range(indices.shape[0]):
+            count_grid[indices[i, 0], indices[i, 1], indices[i, 2]] += 1
+            color_grid[indices[i, 0], indices[i, 1], indices[i, 2]] += colors[i]
+
+        divisor = np.maximum(np.expand_dims(count_grid, -1).astype(np.float32), 1.)
+        color_grid /= divisor
+
+        # X, Y, Z = np.mgrid[min_bound[0]:max_bound[0]:(grid_shape[0] * 1j),
+                           # min_bound[1]:max_bound[1]:(grid_shape[1] * 1j),
+                           # min_bound[2]:max_bound[2]:(grid_shape[2] * 1j)]
+        # ds = count_grid.flatten()
+        # X = X.flatten()[ds > 0]
+        # Y = Y.flatten()[ds > 0]
+        # Z = Z.flatten()[ds > 0]
+        # C = color_grid.reshape(-1, 3)[ds > 0]
+        # # values = np.sin(X*Y*Z) / (X*Y*Z)
+        # # vol = (X - 1)**2 + (Y - 1)**2 + Z**2
+
+        # fig = go.Figure(data=[go.Scatter3d(
+            # x=X.flatten(),
+            # y=Y.flatten(),
+            # z=Z.flatten(),
+            # mode='markers',
+            # marker=dict(
+                # size=grid_shape[0],
+                # color=255 * C,
+                # opacity=1.0,
+            # )
+        # )])
+
+        # fig.write_html('first_figure.html', auto_open=True)
+
+        count_feature_grid = count_grid.astype(np.float32) / 16
+        feature_grid = np.concatenate((color_grid,
+                                       np.expand_dims(count_grid, -1),
+                                       np.ones(grid_shape + (1,))),
+                                      axis=-1,
+                                      dtype=np.float32)
+
+        assert feature_grid.dtype == np.float32
+        assert feature_grid.shape == grid_shape + (VOXEL_FEATURES,)
+        return feature_grid
+
 
 if __name__ == '__main__':
-    render_env('reach')
+    render_env_mj_pc('reach')
--- src/mj_pc/mj_point_clouds.py
+++ src/mj_pc/mj_point_clouds.py
@@ -67,7 +67,7 @@ def rotMatList2NPRotMat(rot_mat_arr):
     return np_rot_mat
 
 """
-Generates numpy transformation matrix from position list len(3) and 
+Generates numpy transformation matrix from position list len(3) and
     numpy rotation matrix
 
 @param pos:     list len(3) containing position
@@ -99,7 +99,7 @@ def cammat2o3d(cam_mat, width, height):
 
     return o3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, cx, cy)
 
-# 
+#
 # and combines them into point clouds
 """
 Class that renders depth images in MuJoCo, processes depth images from
@@ -116,60 +116,84 @@ class PointCloudGenerator(object):
     @param max_bound: If not None, list len(3) containing largest x, y, and z
         values that will not be cropped
     """
-    def __init__(self, sim, min_bound=None, max_bound=None):
+    def __init__(self, sim, min_bound=None, max_bound=None, camera_names=None,
+                 *, width=640, height=480):
         super(PointCloudGenerator, self).__init__()
 
         self.sim = sim
 
-        # I think these can be set to anything
-        self.img_width = 640
-        self.img_height = 480
+        self.img_width = width
+        self.img_height = height
 
-        self.cam_names = self.sim.model.camera_names
+        if camera_names is None:
+            self.cam_names = list(enumerate(self.sim.model.camera_names))
+        else:
+            cam_name_to_id = {name: i for (i, name) in enumerate(self.sim.model.camera_names)}
+            self.cam_names = [(cam_name_to_id[name], name) for name in camera_names]
 
         self.target_bounds=None
         if min_bound != None and max_bound != None:
             self.target_bounds = o3d.geometry.AxisAlignedBoundingBox(min_bound=min_bound, max_bound=max_bound)
 
         # List of camera intrinsic matrices
-        self.cam_mats = []
-        for cam_id in range(len(self.cam_names)):
+        self.cam_mats = {}
+        for (cam_id, cam_name) in self.cam_names:
             fovy = math.radians(self.sim.model.cam_fovy[cam_id])
             f = self.img_height / (2 * math.tan(fovy / 2))
-            cam_mat = np.array(((f, 0, self.img_width / 2), (0, f, self.img_height / 2), (0, 0, 1)))
-            self.cam_mats.append(cam_mat)
+            cam_mat = np.array(((f, 0, self.img_width / 2, 0),
+                                (0, f, self.img_height / 2, 0),
+                                (0, 0, 1, 0),
+                                (0, 0, 0, 1)))
+            self.cam_mats[cam_id] = cam_mat
 
-    def generateCroppedPointCloud(self, save_img_dir=None):
+    def cam2World(self, cam_name, cam_id):
+        cam_body_id = self.sim.model.cam_bodyid[cam_id]
+
+        try:
+            cam_pos = self.sim.data.get_camera_xpos(cam_name)
+        except TypeError:
+            cam_pos = self.sim.model.body_pos[cam_body_id]
+
+        try:
+            cam_mat = self.sim.data.get_camera_xmat(cam_name)
+        except TypeError:
+            cam_mat = self.sim.model.cam_mat0[cam_id]
+        # c2b_r = rotMatList2NPRotMat(self.sim.model.cam_mat0[cam_id])
+        c2b_r = rotMatList2NPRotMat(cam_mat)
+        # In MuJoCo, we assume that a camera is specified in XML as a body
+        #    with pose p, and that that body has a camera sub-element
+        #    with pos and euler 0.
+        #    Therefore, camera frame with body euler 0 must be rotated about
+        #    x-axis by 180 degrees to align it with the world frame.
+        b2w_r = quat2Mat([0, 1, 0, 0])
+        c2w_r = np.matmul(c2b_r, b2w_r)
+        c2w = posRotMat2Mat(cam_pos, c2w_r)
+        return c2w
+
+    def images2PointCloud(self, cam2world_mats, color_imgs, depth_imgs,
+                          cam_locations, estimate_normals=True):
         o3d_clouds = []
-        cam_poses = []
-        for cam_i in range(len(self.cam_names)):
-            # Render and optionally save image from camera corresponding to cam_i
-            depth_img = self.captureImage(cam_i)
-            # If directory was provided, save color and depth images
-            #    (overwriting previous)
-            if save_img_dir != None:
-                self.saveImg(depth_img, save_img_dir, "depth_test_" + str(cam_i))
-                color_img = self.captureImage(cam_i, False)
-                self.saveImg(color_img, save_img_dir, "color_test_" + str(cam_i))
+        for (cam_id, cam_name) in self.cam_names:
+            color_img = color_imgs[cam_name]
+            depth_img = depth_imgs[cam_name]
+            c2w = cam2world_mats[cam_name]
+            cam_pos = cam_locations[cam_name]
 
             # convert camera matrix and depth image to Open3D format, then
             #    generate point cloud
-            od_cammat = cammat2o3d(self.cam_mats[cam_i], self.img_width, self.img_height)
-            od_depth = o3d.geometry.Image(depth_img)
-            o3d_cloud = o3d.geometry.PointCloud.create_from_depth_image(od_depth, od_cammat)
+            od_cammat = cammat2o3d(self.cam_mats[cam_id], self.img_width, self.img_height)
+            # od_rgb = o3d.geometry.Image(color_img)
+            # od_rgb = o3d.geometry.Image(color_img.transpose(1, 0, 2)
+                                        # .reshape(self.img_height, self.img_width, 3))
+            # Open3D Image doesn't know how to handle views, make sure to use a copy
+            od_rgb = o3d.geometry.Image(np.copy(color_img))
+            od_depth = o3d.geometry.Image(np.copy(depth_img))
+            od_rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+                od_rgb, od_depth, depth_scale=1.0, depth_trunc=100.0,
+                convert_rgb_to_intensity=False)
+            o3d_cloud = o3d.geometry.PointCloud.create_from_rgbd_image(od_rgbd, od_cammat)
 
             # Compute world to camera transformation matrix
-            cam_body_id = self.sim.model.cam_bodyid[cam_i]
-            cam_pos = self.sim.model.body_pos[cam_body_id]
-            c2b_r = rotMatList2NPRotMat(self.sim.model.cam_mat0[cam_i])
-            # In MuJoCo, we assume that a camera is specified in XML as a body
-            #    with pose p, and that that body has a camera sub-element
-            #    with pos and euler 0.
-            #    Therefore, camera frame with body euler 0 must be rotated about
-            #    x-axis by 180 degrees to align it with the world frame.
-            b2w_r = quat2Mat([0, 1, 0, 0])
-            c2w_r = np.matmul(c2b_r, b2w_r)
-            c2w = posRotMat2Mat(cam_pos, c2w_r)
             transformed_cloud = o3d_cloud.transform(c2w)
 
             # If both minimum and maximum bounds are provided, crop cloud to fit
@@ -180,6 +204,7 @@ class PointCloudGenerator(object):
             # Estimate normals of cropped cloud, then flip them based on camera
             #    position.
             transformed_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.03, max_nn=250))
+            cam_body_id = self.sim.model.cam_bodyid[cam_id]
             transformed_cloud.orient_normals_towards_camera_location(cam_pos)
 
             o3d_clouds.append(transformed_cloud)
@@ -188,6 +213,41 @@ class PointCloudGenerator(object):
         for cloud in o3d_clouds:
             combined_cloud += cloud
         return combined_cloud
+
+    def generateObservation(self):
+        color_imgs = {}
+        depth_imgs = {}
+        cam2world_mats = {}
+        cam_locations = {}
+        for (cam_id, cam_name) in self.cam_names:
+            color_img, depth_img = self.captureImage(cam_name)
+            color_imgs[cam_name] = color_img
+            depth_imgs[cam_name] = depth_img
+            cam_locations[cam_name] = self.sim.data.get_camera_xpos(cam_name).astype(np.float32)
+            cam2world_mats[cam_name] = self.cam2World(cam_name, cam_id).astype(np.float32)
+        return {
+            'cam2world_mats': cam2world_mats,
+            'cam_locations': cam_locations,
+            'color_imgs': color_imgs,
+            'depth_imgs': depth_imgs,
+        }
+
+    def observationToPointCloud(self, observation, estimate_normals=True):
+        cam2world_mats = observation['cam2world_mats']
+        cam_locations = observation['cam_locations']
+        color_imgs = observation['color_imgs']
+        depth_imgs = observation['depth_imgs']
+        return self.images2PointCloud(cam2world_mats, color_imgs, depth_imgs,
+                                      cam_locations, estimate_normals)
+
+    def generateCroppedPointCloud(self, save_img_dir=None):
+        obs = self.generateObservation()
+        if save_img_dir != None:
+            cam2world_mats, color_imgs, depth_imgs = observation
+            for (cam_id, cam_name) in self.cam_names:
+                self.saveImg(depth_imgs[cam_name], save_img_dir, "depth_test_" + str(cam_id))
+                self.saveImg(color_imgs[cam_name], save_img_dir, "color_test_" + str(cam_id))
+        return self.observationToPointCloud(obs)
 
     # https://github.com/htung0101/table_dome/blob/master/table_dome_calib/utils.py#L160
     def depthimg2Meters(self, depth):
@@ -201,18 +261,16 @@ class PointCloudGenerator(object):
         return np.flip(img, axis=0)
 
     # Render and process an image
-    def captureImage(self, cam_ind, capture_depth=True):
-        rendered_images = self.sim.render(self.img_width, self.img_height, camera_name=self.cam_names[cam_ind], depth=capture_depth)
-        if capture_depth:
-            img, depth = rendered_images
-            depth = self.verticalFlip(depth)
-            real_depth = self.depthimg2Meters(depth)
+    def captureImage(self, cam_name):
+        img, depth = self.sim.render(self.img_width, self.img_height,
+                                     camera_name=cam_name,
+                                     depth=True)
+        # Rendered images appear to be flipped about vertical axis
+        depth = self.verticalFlip(depth)
+        img = self.verticalFlip(img)
+        real_depth = self.depthimg2Meters(depth)
 
-            return real_depth
-        else:
-            img = rendered_images
-            # Rendered images appear to be flipped about vertical axis
-            return self.verticalFlip(img)
+        return img, real_depth
 
     # Normalizes an image so the maximum pixel value is 255,
     # then writes to file
