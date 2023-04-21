6b673d9f6ad8123a40bb3a277d895377382ea550
K.R. Zentner <krzentner@gmail.com>
2023-04-21 10:04:10 -0700

    Add setup_vm.sh script

---
+++ scripts/setup_vm.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash -xe
+sudo apt-get -y -q update
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
+    # Dockerfile deps
+    git \
+    curl \
+    python3.9 \
+    python3.9-venv \
+    python3.9-dev \
+    # mujoco_py
+    # See https://github.com/openai/mujoco-py/blob/master/Dockerfile
+    libglew-dev \
+    libosmesa6-dev \
+    patchelf \
+    # OpenAI baselines
+    libopenmpi-dev \
+    # virtualenv
+    libbz2-dev \
+    libreadline-dev \
+    libssl-dev \
+    libsqlite3-dev \
+    xvfb \
+    pulseaudio
+
+sudo mkdir -p -m 1777 /tmp/.X11-unix
+
+curl -sSL https://install.python-poetry.org | python3 -
+
+mkdir -p $HOME/.mujoco && \
+curl -O https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz && \
+tar xf mujoco210-linux-x86_64.tar.gz --directory $HOME/.mujoco
+
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin' >> ~/.profile
